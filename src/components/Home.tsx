69d6640de9a995141138feff3cef825af1f7f54e
abidurr <42abid@gmail.com>
2020-06-13 02:52:47 -0400

    Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,12 @@
 import React from "react"
 
 export default function Home() {
-    return <div> <h1>Home</h1> </div>
+    return (
+    <div> 
+        <h1>Home</h1>
+        <p>This is the home page of my random content generator.</p>
+        <p>Please select the random content you want to see.</p>
+        <p>This web app was built with TypeScript and Create-React-App.</p>
+    </div>
+    )
 }
