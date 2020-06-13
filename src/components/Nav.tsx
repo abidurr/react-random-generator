0d08ef25d0221dd95d183bb9975784f8abeea49d
abidurr <42abid@gmail.com>
2020-06-12 20:51:21 -0400

    Styling Nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,10 @@
 import React from "react"
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom"
 import StyledNav from "../styles/StyledNav"
 import Pictures from "./Pictures"
 import Colors from "./Colors"
 import Countries from "./Countries"
+import Home from "./Home"
 
 export default function Nav(): JSX.Element {
     return (
@@ -12,23 +13,24 @@ export default function Nav(): JSX.Element {
                 <nav>
                     <ul>
                         <li>
-                            <Link to="/">Home</Link>
+                            <NavLink exact to="/" activeClassName="active">Home</NavLink>
                         </li>
                         <li>
-                            <Link to="/pictures">Pictures</Link>
+                            <NavLink to="/pictures" activeClassName="active">Pictures</NavLink>
                         </li>
                         <li>
-                            <Link to="/countries">Countries</Link>
+                            <NavLink to="/countries" activeClassName="active">Countries</NavLink>
                         </li>
                         <li>
-                            <Link to="/colors">Colors</Link>
+                            <NavLink to="/colors" activeClassName="active">Colors</NavLink>
                         </li>
                     </ul>
                 </nav>
-                    <Route exact path="/pictures" component={Pictures} />
-                    <Route exact path="/colors" component={Colors} />
-                    <Route exact path="/countries" component={Countries} />
             </StyledNav>
+            <Route exact path="/" component={Home} />
+            <Route path="/pictures" component={Pictures} />
+            <Route path="/colors" component={Colors} />
+            <Route path="/countries" component={Countries} />
         </Router>
     )
 }
--- src/styles/StyledNav.tsx
+++ src/styles/StyledNav.tsx
@@ -1,6 +1,31 @@
 import styled from "styled-components"
 
-const Nav = styled.div`
-    
+const StyledNav = styled.nav`
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+    height: 5em;
+    background-color: darkgray;
+
+    ul {
+        justify-content: space-around;
+    }
+
+    ul li {
+        list-style-type: none;
+        display: inline-block;
+    }
+
+    ul li a {
+        text-decoration: none;
+        color: white;
+        padding: 5px;
+    }
+
+    .active {
+        color: black;
+    }
 `
-export default Nav
+export default StyledNav
